8ce634d982d146d2c7e94501f79017835176399d
gabidavila <gabidavila@gmail.com>
2017-09-02 14:26:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const app = "I don't do much."
+'use strict';
+
+function Rectangule(sides, width, height) {
+  this.sides = sides;
+  this.width = witdh;
+  this.height = height;
+}
+
+Rectangule.prototype.area = function() {
+  return this.width * this.height;
+}
+
+Rectangule.prototype.perimeter = function() {
+  return (this.width + this.height) * 2;
+}
+
+function Square(sides, length) {
+  this.sides = sides
+  this.height = length
+  this.width = length
+}
+
+Square.prototype = Object.create(Rectangule.prototype)
+
+Rectangule.prototype.internalAngules = 90
